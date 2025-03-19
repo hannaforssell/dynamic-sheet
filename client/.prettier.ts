a2e594da39de1d08575050f3f9a5bad680f2bd0c
joar.selving@gmail.com <joar.selving@gmail.com>
2025-03-19 08:20:47 +0100

    Wip

---
+++ client/.prettier.ts
@@ -0,0 +1,20 @@
+// /** @type {import("prettier").Config} */
+// const config = {
+//     trailingComma: "es5",
+//     tabWidth: 4,
+//     semi: false,
+//     singleQuote: true,
+// };
+
+// export default config;
+
+import { type Config } from "prettier";
+
+const config: Config = {
+  // trailingComma: "none",
+  trailingComma: "es5",
+  tabWidth: 4,
+  singleQuote: true,
+};
+
+export default config;
